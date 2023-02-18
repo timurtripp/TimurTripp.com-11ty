abe91c399a12a62371ac220966d8530b54819c53
Timur Tripp <timur@timurtripp.com>
2023-02-17 20:36:01 -0700

    Updates portfolio term system

--- src/data-generators.js
+++ src/data-generators.js
@@ -8,24 +8,37 @@ module.exports = {
 			index = yaml.load(fs.readFileSync('_data/portfolio/index.yml')),
 			returnObject = {};
 		for (const [dictionaryTermListingId, dictionaryTermListing] of Object.entries(dictionary)) {
-			if (dictionaryTermListingId[0] == '_') continue;
 			const inclusion = returnObject[dictionaryTermListingId] = {};
 			for (const tid of Object.keys(dictionaryTermListing))
-				inclusion[tid] = {};
+				inclusion[tid] = {dataInclusion: {}, termInclusion: {}};
+			if (dictionaryTermListingId[0] === '_') continue;
 			index.forEach(indexItem => {
 				const dataListing = data[indexItem.id];
 				for (const [dataItemId, dataItem] of Object.entries(dataListing)) {
 					if(dataItem && dataItem['_' + dictionaryTermListingId]) {
 						dataItem['_' + dictionaryTermListingId].forEach(tid => {
-							if(inclusion[tid]) {
-								const dataItemTermInclusion = inclusion[tid][indexItem.id] || [];
+							if(inclusion[tid].dataInclusion) {
+								const dataItemTermInclusion = inclusion[tid].dataInclusion[indexItem.id] || [];
 								dataItemTermInclusion.push(dataItemId);
-								inclusion[tid][indexItem.id] = dataItemTermInclusion;
+								inclusion[tid].dataInclusion[indexItem.id] = dataItemTermInclusion;
 							}
 						})
 					}
 				}
 			});
+		}
+		for (const [collectionid, collection] of Object.entries(dictionary)) {
+			for (const [termid, term] of Object.entries(collection)) {
+				if(term.descriptors) {
+					term.descriptors.forEach(descriptor => {
+						if(returnObject[descriptor.collection] && returnObject[descriptor.collection][descriptor.term]) {
+							const termCollectionInclusion = returnObject[descriptor.collection][descriptor.term].termInclusion[collectionid] || [];
+							termCollectionInclusion.push(termid);
+							returnObject[descriptor.collection][descriptor.term].termInclusion[collectionid] = termCollectionInclusion;
+						}
+					});
+				}
+			}
 		}
 		return returnObject;
 	}
