4376af55a4ec787e5aaec8b4740accd5e34ab64b
Timur Tripp <timur@timurtripp.com>
2023-07-29 18:31:35 -0600

    Modifies site description; modifies titles of term pages

--- _data/eleventyComputed.js
+++ _data/eleventyComputed.js
@@ -1,11 +1,19 @@
 const package = require('../package.json');
 const { TermFactory } = require('../src/term.js');
 
+function termTitle(data) {
+	const
+		dictionary = data[data.termroot].dictionary,
+		termDefinition = dictionary[data.termcategoryid][data.termid],
+		indexDefinition = dictionary._index[data.termcategoryid];
+	return (indexDefinition ? indexDefinition.label + ': ' : '') + termDefinition.label;
+}
+
 module.exports = {
 	isDeveloperPage: data => data.page.inputPath.indexOf('/README') > -1 || data.page.inputPath.indexOf('/LICENSE') > -1 || data.page.inputPath.indexOf('/TEST') > -1,
 	name: data => data.site.name || package.name,
 	title: data => {
-		const pageTitle = data.isDeveloperPage ? data.page.filePathStem.substr(1) + ' (Source Project for Developers)' : data.title;
+		const pageTitle = data.isDeveloperPage ? data.page.filePathStem.substr(1) + ' (Source Project for Developers)' : data.termid ? termTitle(data) : data.title;
 		return (pageTitle ? pageTitle + ' | '  : '') + data.name;
 	},
 	author: data => data.author || data.site.author || package.author,
